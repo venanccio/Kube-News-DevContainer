7293388569d0792c7653cf838688c2e457c0636d
venanccio <rodrigo.venancio@outlook.com>
2025-05-08 22:19:48 +0000

    Atualizando:  Desafio 2: - Melhorando o Ambiente com Extensões e Ferramentas

---
+++ .devcontainer/ init-project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# init-project.sh
+echo "Instalando dependências do projeto..."
+npm install
+echo "Configurando ambiente de desenvolvimento..."
+# Adicione aqui outros comandos de inicialização, se necessário
+echo "Ambiente pronto para desenvolvimento!"
+Não se esqueça de tornar o script executável:
+bash
+chmod +x .devcontainer/init-project.sh
